777756345723798f0dff45bf17cd5fcad5a90a99
Haami <szymontok4rski@gmail.com>
2020-08-26 22:11:14 +0200

    init push

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const port = process.env.PORT || 8000;
 
-const app = express()
+const app = express();
+//Static Files
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname + 'public/css'))
+
+
+
 app.set('view engine', 'hbs');
+app.use()
 app.get('/', function(req,res){
     res.render('index',{
         pageTitle: 'Centrica Break Tool'
